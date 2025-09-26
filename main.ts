e47e6bd33143b4a7e4230b32361e806f4f241354
default <ketia.ineza@stu.ocsb.ca>
2025-09-26 15:41:07 +0000

    Changed program to print "I love Computer Science"

---
+++ main.ts
@@ -0,0 +1,17 @@
+/**
+* @author Kim In Ju
+* @version 1.0.0
+* @date 2025-09-26
+* @fileoverview Program to practice string literals
+*/
+
+// string literal text
+console.log("I love Computer Science!!");
+
+//string literal text
+console.log("    I love Computer Science");
+
+//string literal text
+console.log("I   Computer   Science   100%");
+
+console.log("\nDone.");
